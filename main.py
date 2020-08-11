96c399192e93800d141941ed491dcb3fd3d6ce72
Nicolas Terroni <nsterroni@gmail.com>
2020-08-11 20:53:16 -0300

    search client function

--- main.py
+++ main.py
@@ -35,10 +35,28 @@ def delete_client(client_name):
     global clients
 
     if client_name.capitalize() in clients:
-        clients = clients.replace(client_name.capitalize() + ",","")
-        list_clients()
+        ask_again = input(f"Are you sure? ´{client_name.capitalize()}´ will be erased. (Y/N): ")
+        if ask_again.upper() == "Y": 
+            clients = clients.replace(client_name.capitalize() + ",","")
+            list_clients()
+        elif ask_again.upper() == "N":
+            print("Canceling.")
+        else:
+            print("Unknown command.")
     else:
         print("Client is not in clients list.")
+
+
+def search_client(client_name):
+    global clients
+    
+    clients_list = clients.split(",") 
+
+    for client in clients_list:
+        if client != client_name.capitalize():
+            continue
+        else:
+            return True
 
 
 def _get_client_name():
@@ -50,11 +68,12 @@ def _print_welcome():
     print("*" * 50)
     print("Choose an option:")
     print("1) CREATE CLIENT")
-    print("2) SHOW CLIENTS")
+    print("2) LIST CLIENTS")
     print("3) UPDATE CLIENT")
     print("4) DELETE CLIENT")
+    print("5) SEARCH CLIENT")
     print("*" * 50)
-    print("5) EXIT")
+    print("6) EXIT")
     print("*" * 50)
 
 
@@ -79,6 +98,17 @@ if __name__ == "__main__":
             client_name = _get_client_name()
             delete_client(client_name)
         elif command == "5":
+            client_name = _get_client_name()
+            found = search_client(client_name)
+            if found:
+                print("-" *50)
+                print(f"´{client_name.capitalize()}´ is in the client´s list.")
+                print("-" *50)
+            else:
+                print("-" *50)
+                print(f"´{client_name.capitalize()}´ is not in the client´s list.")
+                print("-" *50)
+        elif command == "6":
             print("")
             print("Thank you for choosing us.")
             print("")
