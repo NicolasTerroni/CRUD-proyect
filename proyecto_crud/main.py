cdacbc28bb1edc64c59500a56d08c8c162bee331
Nicolas Terroni <nsterroni@gmail.com>
2020-08-11 17:47:15 -0300

    delete function

--- proyecto_crud/main.py
+++ proyecto_crud/main.py
@@ -31,6 +31,16 @@ def update_client(client_name, updated_client_name):
         print("Client is not in clients list.")
 
 
+def delete_client(client_name):
+    global clients
+
+    if client_name.capitalize() in clients:
+        clients = clients.replace(client_name.capitalize() + ",","")
+        list_clients()
+    else:
+        print("Client is not in clients list.")
+
+
 def _get_client_name():
     return  input("Input client´s name: ")
 
@@ -43,8 +53,9 @@ def _print_welcome():
     print("2) SHOW CLIENTS")
     print("3) UPDATE CLIENT")
     print("4) DELETE CLIENT")
-    
+    print("*" * 50)
     print("5) EXIT")
+    print("*" * 50)
 
 
 if __name__ == "__main__":
@@ -65,7 +76,8 @@ if __name__ == "__main__":
             updated_client_name = input("Input the updated client name: ")
             update_client(client_name,updated_client_name)
         elif command == "4":
-            pass
+            client_name = _get_client_name()
+            delete_client(client_name)
         elif command == "5":
             print("")
             print("Thank you for choosing us.")
